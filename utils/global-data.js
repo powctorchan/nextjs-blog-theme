07fa3e9062991d89873a3e988f1d9c832821e6e4
PowctoRhan <75431295+powctorchan@users.noreply.github.com>
2022-04-06 22:30:38 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Learing Notes';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'DRIVEN BY PASSION';
+    : 'DRIVEN BY Intérêt';
 
   return {
     name,
